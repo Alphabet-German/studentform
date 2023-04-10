80ec9bbce6d62057dff3c09b4c05bf8b32513fbc
Alphabet-German <130347376+Alphabet-German@users.noreply.github.com>
2023-04-10 13:37:09 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -14,12 +14,12 @@ st.title(pageTitle + ' ' + pageIcon)
 with st.form('entryForm', clear_on_submit=True):
     # col0, col1 = st.columns(2)
     # col0.text('Name')
-    st.text_input('Name', placeholder='Enter Name', label_visibility='collapsed')
+    st.text_input('Name', placeholder='Enter Name')
     # st.text('Address')
-    st.text_input('Address', placeholder='Enter Address', label_visibility='collapsed')
+    st.text_input('Address', placeholder='Enter Address')
     # col0.text('Course')
     courses = ['8th', '9th', '10th', 'A1']
-    st.selectbox('Course', courses, label_visibility='collapsed')
+    st.selectbox('Course', courses)
 
 
     "---"
