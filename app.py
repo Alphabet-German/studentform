6d141585bb12a5041dd050946d8022b190ad4627
Alphabet-German <130347376+Alphabet-German@users.noreply.github.com>
2023-04-11 14:12:01 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import database as db
 
 pageTitle = 'Alphabet German Academy Admission Form'
 pageIcon = ':monkey_face:'
@@ -14,17 +15,20 @@ st.title(pageTitle + ' ' + pageIcon)
 with st.form('entryForm', clear_on_submit=True):
     # col0, col1 = st.columns(2)
     # col0.text('Name')
-    st.text_input('Name', placeholder='Enter Name')
+    st.text_input('Name', placeholder='Enter Name', key='name')
     # st.text('Address')
-    st.text_input('Address', placeholder='Enter Address')
+    st.text_input('Address', placeholder='Enter Address', key='addr')
     # col0.text('Course')
     courses = ['8th', '9th', '10th', 'A1']
-    st.selectbox('Course', courses)
-
+    st.selectbox('Course', courses, key='course')
 
     "---"
 
     submitted = st.form_submit_button('Save Form')
 
     if submitted:
+        name = st.session_state['name']
+        addr = st.session_state['addr']
+        course = st.session_state['course']
+        db.insertUser(name, addr, course)
         st.success('Data Saved')
---
+++ database.py
@@ -0,0 +1,13 @@
+from deta import Deta
+import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+DETA_KEY = os.getenv('DETA_KEY')
+
+deta = Deta(DETA_KEY)
+
+db = deta.base('userformdb')
+
+def insertUser(name, addr, course):
+    return db.put({'name': name, 'addr': addr, 'course': course})
